3e51772210d28463be5585cb258754161cd43eaa
Andreas Mautsch <amautsch>
2022-12-01 15:48:04 +0100

    port changed to 50900

--- go/callee-service/build.sh
+++ go/callee-service/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 GOOS=linux GOARCH=arm64 go build .
-docker build --tag goafabric/callee-service-go:1.0.0 .
+docker build -f docker/Dockerfile --tag goafabric/callee-service-go:1.0.0 .
+docker push goafabric/callee-service-go:1.0.0
 rm ./callee-service
-docker run --rm --name callee-service -p50900:50900 goafabric/callee-service-go:1.0.0
+docker run --rm --name callee-service-go -p50900:50900 goafabric/callee-service-go:1.0.0
