ae3733d41d35198ad4ea0d522fae80d589b59676
Andreas Mautsch <amautsch>
2022-12-11 16:23:29 +0100

    changes

--- go/restservice.go
+++ go/restservice.go
@@ -3,11 +3,11 @@ package main
 import "net/http"
 
 func main() {
-    println("starting to serve you on http://localhost:8080/catalog/products")
-	http.HandleFunc("/catalog/products", productsHandler)
+    println("starting to serve you on http://localhost:8080/hello")
+	http.HandleFunc("/hello", productsHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func productsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Schuhe, Hose, Hemd"))
+	w.Write([]byte("Hello from Go"))
 }
