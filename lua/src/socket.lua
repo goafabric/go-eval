f814c7e1d5d0e284f7110330a7dd2e69fc5172d7
Andreas Mautsch <amautsch>
2025-01-25 07:31:07 +0100

    changes

--- lua/src/socket.lua
+++ lua/src/socket.lua
@@ -3,25 +3,17 @@
 local http = require("socket.http")
 local ltn12 = require("ltn12")
 
--- URL to call
-local url = "http://localhost:50900/callees/sayMyName?name=Heisenberg"
 
--- Buffer to hold the response
+local url = "http://localhost:50900/callees/sayMyName?name=Heisenberg"
 local response = {}
 
--- Perform the GET request
 local res, status_code, headers, status = http.request{
     url = url,
     sink = ltn12.sink.table(response)
 }
 
--- Check the status code
 if status_code == 200 then
-    -- Print the response
-    print("Response:")
     print(table.concat(response))
 else
-    print("HTTP Request failed!")
-    print("Status code:", status_code)
-    print("Status:", status)
+    print("HTTP Request failed!", status_code, status)
 end
