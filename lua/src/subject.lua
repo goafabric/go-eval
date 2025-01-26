a40b0ed2749cfbc7c190970ada7e8fe5ff6f3928
Andreas Mautsch <amautsch>
2025-01-26 09:10:09 +0100

    changes

--- lua/src/subject.lua
+++ lua/src/subject.lua
@@ -1,15 +1,10 @@
 -- sudo luarocks install lua-resty-jwt
 -- sudo luarocks install lua-cjson
 local jwt = require "resty.jwt"  -- Import the lua-resty-jwt library
-
 local x_access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+local jwt_obj = jwt:verify("lua-resty-jwt", x_access_token)
 
--- Decode the JWT token
-local jwt_obj = jwt:decode(x_access_token)
-
--- Check if decoding was successful
 if jwt_obj then
-    -- Access the 'sub' field
     local sub = jwt_obj.payload.sub
     ngx.say("sub: " .. sub)
 else
