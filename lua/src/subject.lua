cadb8fec210cd6ed215120e85ad0f52c4721b1ed
Andreas Mautsch <amautsch>
2025-01-27 08:40:02 +0100

    changes

--- lua/src/subject.lua
+++ lua/src/subject.lua
@@ -1,12 +1,4 @@
--- sudo luarocks install lua-resty-jwt
--- sudo luarocks install lua-cjson
-local jwt = require "resty.jwt"  -- Import the lua-resty-jwt library
-local x_access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-local jwt_obj = jwt:verify("lua-resty-jwt", x_access_token)
-
-if jwt_obj then
-    local sub = jwt_obj.payload.sub
-    ngx.say("sub: " .. sub)
-else
-    ngx.say("Invalid token")
-end
+local jwt = require "resty.jwt"
+local jwt_obj = jwt:verify("lua-resty-jwt", ngx.req.get_headers()["x-access-token"])
+local sub = jwt_obj.payload.sub
+ngx.log(ngx.ERR, "subject is: ", sub)
