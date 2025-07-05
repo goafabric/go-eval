a33b6677d2267645c07b9a9f78f18d97d1c306c3
Andreas Mautsch <amautsch>
2025-07-05 21:41:35 +0200

    chang

---
+++ go/callee-service-gofr/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+GOOS=linux GOARCH=arm64 go build .
+docker build -f docker/Dockerfile --tag goafabric/callee-service-go:1.0.1 .
+docker push goafabric/callee-service-go:1.0.1
+rm ./callee-service
+docker run --rm --name callee-service-go -p50900:50900 goafabric/callee-service-go:1.0.1
