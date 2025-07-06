fde00cae592faf8f6b57895ff7543f8830cd44ed
Andreas Mautsch <amautsch>
2025-07-06 10:47:24 +0200

    chang

--- go/callee-service-gofr/persistence/personrepository.go
+++ go/callee-service-gofr/persistence/personrepository.go
@@ -16,8 +16,12 @@ func RoutePerson(app *gofr.App) {
 }
 
 func demoData(ctx *gofr.Context) (error) {
-    err := save(ctx, Person{ID: uuid.New().String(), OrganizationId: "1", FirstName: "Homer", LastName: "Simpson"})
-    return err
+    err := createPersonTable(ctx)
+    if err != nil {
+        return err
+    }
+    err2 := save(ctx, Person{ID: uuid.New().String(), OrganizationId: "1", FirstName: "Homer", LastName: "Simpson"})
+    return err2
 }
 
 func save(ctx *gofr.Context, person Person) (error) {
@@ -25,6 +29,23 @@ func save(ctx *gofr.Context, person Person) (error) {
 	    person.ID, person.OrganizationId, person.FirstName, person.LastName)
 
     return err
+}
+
+func createPersonTable(ctx *gofr.Context) error {
+	query := `
+	CREATE SCHEMA IF NOT EXISTS example_0;
+
+	CREATE TABLE IF NOT EXISTS example_0.person (
+		id VARCHAR(36) NOT NULL PRIMARY KEY,
+		organization_id VARCHAR(36) NOT NULL,
+		first_name VARCHAR(255),
+		last_name VARCHAR(255),
+		version BIGINT DEFAULT 0
+	);
+	`
+
+	_, err := ctx.SQL.ExecContext(ctx, query)
+	return err
 }
 
 func findAll(ctx *gofr.Context) (any, error) {
