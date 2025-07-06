a13e03f512316c7eaeb93d257e10c8ec26895e07
Andreas Mautsch <amautsch>
2025-07-06 16:23:16 +0200

    changes

--- go/callee-service-gofr/controller/calleecontroller.go
+++ go/callee-service-gofr/controller/calleecontroller.go
@@ -1,10 +1,13 @@
 package controller
 
-import "gofr.dev/pkg/gofr"
-import logic "callee-service/logic"
+import (
+	logic "callee-service/logic"
+
+	"gofr.dev/pkg/gofr"
+)
 
 func RouteCallee(app *gofr.App) {
-    app.GET("/callees/sayMyName", func(ctx *gofr.Context) (interface{}, error) {
+	app.GET("/callees/sayMyName", func(ctx *gofr.Context) (interface{}, error) {
 		name := ctx.Param("name")
 		return logic.SayMyName(name), nil
 	})
@@ -14,15 +17,14 @@ func RouteCallee(app *gofr.App) {
 		return logic.SayMyOtherName(name), nil
 	})
 
-    /*
-	app.POST("/callees/save", func(ctx *gofr.Context) (interface{}, error) {
-		var callee Callee
-        if err := ctx.Bind(&callee); err != nil {
-            return nil, gofr.NewError(http.StatusBadRequest, "invalid request body")
-        }
-		return Callee{ID: "0", Message: "Storing your message: " + callee.Message}
-	})
-    */
+	/*
+			app.POST("/callees/save", func(ctx *gofr.Context) (interface{}, error) {
+				var callee Callee
+		        if err := ctx.Bind(&callee); err != nil {
+		            return nil, gofr.NewError(http.StatusBadRequest, "invalid request body")
+		        }
+				return Callee{ID: "0", Message: "Storing your message: " + callee.Message}
+			})
+	*/
 
 }
-
--- go/callee-service-gofr/main.go
+++ go/callee-service-gofr/main.go
@@ -1,24 +1,25 @@
 package main
 
-import "gofr.dev/pkg/gofr"
+import (
+	"gofr.dev/pkg/gofr"
 
-import controller "callee-service/controller"
-import extensions "callee-service/extensions"
+	controller "callee-service/controller"
 
-import persistence "callee-service/persistence"
+	extensions "callee-service/extensions"
 
+	persistence "callee-service/persistence"
+)
 
 func main() {
-    app := gofr.New()
+	app := gofr.New()
 
-    controller.RouteCallee(app)
-    controller.RouteHealth(app)
+	controller.RouteCallee(app)
+	controller.RouteHealth(app)
 
-    persistence.RoutePerson(app)
+	persistence.RoutePerson(app)
 
-    app.UseMiddleware(extensions.PreHandle())
+	app.UseMiddleware(extensions.PreHandle())
 
-    app.AddStaticFiles("/", "./static")
-    app.Run()
+	app.AddStaticFiles("/", "./static")
+	app.Run()
 }
-
