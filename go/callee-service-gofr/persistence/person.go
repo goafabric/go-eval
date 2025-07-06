ac4e485214bbccd7efd8065a38d44d7534a1ebf7
Andreas Mautsch <amautsch>
2025-07-06 10:34:33 +0200

    chang

--- go/callee-service-gofr/persistence/person.go
+++ go/callee-service-gofr/persistence/person.go
@@ -2,6 +2,7 @@ package persistence
 
 type Person struct {
 	ID   string   `json:"id"`
+	OrganizationId   string   `json:"organization_id"`
 
 	FirstName string  `json:"first_name"`
 	LastName  string  `json:"last_name"`
--- go/callee-service-gofr/persistence/personrepository.go
+++ go/callee-service-gofr/persistence/personrepository.go
@@ -1,16 +1,31 @@
 package persistence
 
 import (
-
 	"gofr.dev/pkg/gofr"
+	"github.com/google/uuid"
 )
 
 func RoutePerson(app *gofr.App) {
     app.GET("/persons", func(ctx *gofr.Context) (interface{}, error) {
+        err := demoData(ctx)
+        if err != nil {
+            return nil, err
+        }
         return findAll(ctx)
 	})
 }
 
+func demoData(ctx *gofr.Context) (error) {
+    err := save(ctx, Person{ID: uuid.New().String(), OrganizationId: "1", FirstName: "Homer", LastName: "Simpson"})
+    return err
+}
+
+func save(ctx *gofr.Context, person Person) (error) {
+	_, err := ctx.SQL.ExecContext(ctx, "INSERT INTO example_0.person (id, organization_id, first_name, last_name) VALUES ($1, $2, $3, $4)",
+	    person.ID, person.OrganizationId, person.FirstName, person.LastName)
+
+    return err
+}
 
 func findAll(ctx *gofr.Context) (any, error) {
     var persons []Person
