18171439f47c37b4eda13e49adaadbc86764823e
Andreas Mautsch <amautsch>
2025-07-06 11:14:18 +0200

    chang

--- go/callee-service-gofr/_docker/build.sh
+++ go/callee-service-gofr/_docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
 GOOS=linux GOARCH=arm64 go build .
-docker build -f docker/Dockerfile --tag goafabric/callee-service-go:1.0.1 .
+docker build -f _docker/Dockerfile --tag goafabric/callee-service-go:1.0.1 .
 docker push goafabric/callee-service-go:1.0.1
 rm ./callee-service
-docker run --rm --name callee-service-go -p50900:50900 goafabric/callee-service-go:1.0.1
+docker run --rm --name callee-service-go -p50900:50900 -e 'DB_HOST=192.168.4.101' goafabric/callee-service-go:1.0.1
